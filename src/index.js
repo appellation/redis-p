683d88d3c3726bbe3c64907ad74b45e577b32105
Will Nelson <nelson.will@live.com>
2017-10-24 14:47:38 -0700

    finish whitelisting

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 const redis = require('redis');
 const { promisify } = require('tsubaki');
 
-const props = [
-  'monitor',
-  'info',
-  'ping',
-  'publish',
-  'auth',
-  'client',
-  'hmset',
-  'subscribe',
-  'unsubscribe',
-  'psubscribe',
-  'punsubscribe',
-  'append',
-  'bgwriteaof',
-  'bgsave',
-  'bitcount',
-  'bitfield',
-  'bitop',
-  'bitpos',
-  'blpop',
-  'brpop',
-  'brpoplpush',
-  'cluster',
-  'command',
-  'config',
-  'dbsize',
-  'debug',
-  'decr',
-  'decrby',
-  'del',
-  'discard',
-];
+const props = require('./cmds.json');
 
 function promisifyAll(obj) {
   for (const k of props) {
